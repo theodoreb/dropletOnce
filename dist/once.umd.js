82d429c3d53ea384eaa7692ee808f15389c2a55a
Théodore BIADALA <theodore@biadala.net>
2020-11-29 09:43:12 +0100

    3.5.1

--- dist/once.umd.js
+++ dist/once.umd.js
@@ -1,4 +1,4 @@
-/*! once - v3.5.0 - 2020-11-29 */
+/*! once - v3.5.1 - 2020-11-29 */
 (function (global, factory) {
   typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
   typeof define === 'function' && define.amd ? define(factory) :
